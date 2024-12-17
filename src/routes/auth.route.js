ea61060cc4ab81624887fb61fdaa22902c535a2c
Nguyen Duc Tri <113581831+sh1nata-piash1nlin@users.noreply.github.com>
2024-12-17 19:47:13 +0700

    Update auth.route.js 17/12 7:45PM

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -15,14 +15,18 @@ router.get('/google/callback',
         if (!req.user) {
             return res.redirect('/login-failure'); // Xử lý nếu không có user
         }
+
+        req.session.isAuthenticated = true;
+
+        req.session.authUser = {
+            avatar: '/static/img/default.png' // Use default avatar if none provided
+        };
         // Điều hướng đến URL client với ID người dùng từ Google
         //  q res.redirect(`${process.env.URL_CLIENT}/login-success/${req.user.id}`);
-        res.redirect('/');
+        res.redirect('/'); 
 
     }
 );
-
-
 
 router.get('/facebook',
     passport.authenticate('facebook', { scope: ['public_profile', 'email'], session: false }))
@@ -42,10 +46,27 @@ router.get('/facebook/callback',
         if (!req.user) {
             return res.redirect('/login-failure'); 
         }
+
+        req.session.isAuthenticated = true;
+
+        req.session.authUser = {
+            avatar: '/static/img/default.png' // Use default avatar if none provided
+        };
         res.redirect('/');
     }
 );
 
 router.post('/login-success', authController.loginSuccess);
+
+router.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.redirect('/');
+        }
+
+        res.clearCookie('connect.sid'); // if using express-session
+        res.redirect('/');  // Redirect to homepage after logout
+    });
+});
 
 module.exports = router;
